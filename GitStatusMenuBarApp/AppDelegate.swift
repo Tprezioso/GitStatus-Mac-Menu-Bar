0d22495394f509fb25842380112d3023f93527f2
Tprezioso <tommyprezioso@gmail.com>
2017-10-06 16:11:11 -0400

    added temp image and added code to show menu bar app

--- GitStatusMenuBarApp/AppDelegate.swift
+++ GitStatusMenuBarApp/AppDelegate.swift
@@ -15,6 +15,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
