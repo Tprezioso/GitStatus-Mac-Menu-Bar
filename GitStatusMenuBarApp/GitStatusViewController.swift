83b9e7d3410695a3b0def5c4b33625c6cd40c8f0
Tprezioso <tommyprezioso@gmail.com>
2017-10-08 22:01:10 -0400

    added viewController for gitStatus

---
+++ GitStatusMenuBarApp/GitStatusViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GitStatusViewController.swift
+//  GitStatusMenuBarApp
+//
+//  Created by Thomas Prezioso on 10/8/17.
+//  Copyright © 2017 Thomas Prezioso. All rights reserved.
+//
+
+import Cocoa
+
+class GitStatusViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
