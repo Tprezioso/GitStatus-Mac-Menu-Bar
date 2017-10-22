e538889cb065c7a7e4094bd32cb20758f28a2cc8
Tprezioso <tommyprezioso@gmail.com>
2017-10-22 16:25:32 -0400

    added button and event monitoring

---
+++ GitStatusMenuBarApp/View/EventMonitor.swift
@@ -0,0 +1,35 @@
+//
+//  EventMonitor.swift
+//  GitStatusMenuBarApp
+//
+//  Created by Thomas Prezioso on 10/22/17.
+//  Copyright © 2017 Thomas Prezioso. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
+    
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+        self.mask = mask
+        self.handler = handler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+    }
+    
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
