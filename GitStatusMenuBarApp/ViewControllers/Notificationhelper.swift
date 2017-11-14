bf13e3a66c3673abd91b29764816779bd84c7fda
Tprezioso <tommyprezioso@gmail.com>
2017-11-14 17:35:39 -0500

    added helper notification for app

---
+++ GitStatusMenuBarApp/ViewControllers/Notificationhelper.swift
@@ -0,0 +1,24 @@
+//
+//  Notificationhelper.swift
+//  GitStatusMenuBarApp
+//
+//  Created by Thomas Prezioso on 11/14/17.
+//  Copyright © 2017 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+
+class NotificationHelper {
+    static func sampleNotification(notification: NSUserNotification) {
+        let notificationCenter = NSUserNotificationCenter.default
+        
+        notification.identifier = "unique-id-123"
+        notification.hasActionButton = true
+        notification.otherButtonTitle = "Close"
+        notification.actionButtonTitle = "Show"
+        notification.title = "Hello"
+        notification.subtitle = "How are you?"
+        notification.informativeText = "This is a test"
+        notificationCenter.deliver(notification)
+    }
+}
